6cbaa826726c43130612c58c30cdd5dac582660b
Chinmay Gupta <f20220452@goa.bits-pilani.ac.in>
2026-09-09 00:16:18 +0530

    Close intruction created and tested.

--- programs/q3_26_vault/src/instructions.rs
+++ programs/q3_26_vault/src/instructions.rs
@@ -1,9 +1,9 @@
+pub mod close;
 pub mod deposit;
 pub mod initialize;
 pub mod withdraw;
-// pub mod close;
 
+pub use close::*;
 pub use deposit::*;
 pub use initialize::*;
 pub use withdraw::*;
-// pub use close::*;
